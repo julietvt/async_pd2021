33085a9a36eefebf63b9ea05eafda60080b023f4
julietvt <freshitpt@gmail.com>
2021-11-27 13:48:47 +0200

    load users card with fetch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,94 +1,41 @@
-// Асинхронный код выполняется только после синхронного!!!
-// status 200 *, 404-not found
-
-/*
-'use strict';
-
-const store = {
-  users: null,
-  isFetching: false,
-  error: null,
-};
-
-const request = new XMLHttpRequest();
-
-const btn = document.getElementById('loadUsers');
-
-btn.onclick = function () {
-  // false - синхронно, true - ассинхронно
-  request.open('GET', '../../assets/data/users.json', true);
-  request.send();
-};
-
-request.onloadstart = function () {
-  store.isFetching = true;
-  //console.log(store);
-};
-
-request.onloadend = function () {
-  store.isFetching = false;
-  if (this.status >= 200 && this.status < 300) {
-    store.users = JSON.parse(this.responseText);
-  } else {
-    store.error = new Error(`${this.status} ${this.statusText}`);
-  }
-};
-
-*/
-
-/*
 fetch('../../assets/data/users.json')
   .then(loadUsersResolve)
   .then(logData)
-  .catch(loadUsersReject);
+  .catch(console.error);
 
 function loadUsersResolve(response) {
   return response.json();
 }
 
-function logData(data) {
-  console.log(data);
+function logData(users) {
+  // обработка данных
+  console.log(users);
+  const list = document.getElementById('usersList');
+  users.forEach((u) => {
+    list.append(createUserCard(u));
+  });
 }
 
-function loadUsersReject(response) {
-  console.log(response);
-}
+function createUserCard(u) {
+  const item = document.createElement('li');
+  item.classList.add('userItem');
 
-/*
-function loadUsersResolve(response) {
-  console.log(response);
-  const loadUsers = response.json();
-  console.log(loadUsers);
-}
+  // image
+  const userImgCont = document.createElement('div');
+  userImgCont.classList.add('imgContainer');
 
-function loadUsersReject(response) {
-  console.log(response);
-}
-*/
+  const userImg = document.createElement('img');
+  userImg.setAttribute('src', u.img);
+  userImg.setAttribute('alt', 'user photo');
+  userImg.classList.add('userImg');
 
-// Promise
+  userImgCont.append(userImg);
+  item.append(userImgCont);
+  // text
+  const userFullName = document.createElement('p');
+  userFullName.innerText = `${u.name} ${u.surname}`;
+  userFullName.classList.add('userFullName');
+  item.append(userFullName);
 
-//1
-
-const promise1 = new Promise(fun1);
-
-function fun1(resolve, reject) {
-  console.log("I'm running");
-}
-
-// 2
-
-const promise2 = new Promise(function (resolve, reject) {
-  console.log("I'm running");
-});
-
-// Получить число меньше 5. Создать обещание
-
-let promiseGetNumberLessThan5 = new Promise(funFor5);
-
-function funFor5(resolve, reject) {
-  let number = Math.random() * 10;
-  console.log('number=', number);
-  if (number < 5) resolve(number);
-  else reject(new Error('Reject. Number is ' + number));
+  return item;
 }
