383659fba37d3ba7e20e8ed257478672236e8043
julietvt <freshitpt@gmail.com>
2021-11-27 10:27:39 +0200

    set interval and timeout examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/*
 const spinner = document.getElementById('spinner');
 let d = 0;
 
@@ -12,3 +13,31 @@ btn.onclick = function () {
   //console.log('Hello');
   alert('Hello');
 };
+*/
+
+// setTimeout, setInterval
+
+//1
+/*
+setTimeout(() => {
+  console.log('Hello');
+}, 2 * 1000);
+*/
+
+//2
+/*
+const fun = (who) => {
+  console.log('Hello, ' + who + ' !!!');
+};
+
+setTimeout(fun, 3 * 1000, 'Vasya');
+setTimeout(fun, 4 * 1000, 'Tom');
+setTimeout(fun, 1 * 1000, 'Masha');
+*/
+
+const fun2 = () => {
+  console.log('Hello');
+};
+
+const id = setTimeout(fun2, 0);
+//clearInterval(id);
