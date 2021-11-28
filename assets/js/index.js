f7685104ae1020b275654e22ad61db27ad7cc10c
julietvt <freshitpt@gmail.com>
2021-11-28 13:55:32 +0200

    pagination user cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,272 +1,68 @@
-/*
-fetch('../../assets/data/users.json')
-  .then(loadUsersResolve)
-  .then(logData)
-  .catch(console.error);
+'use strict';
 
-function loadUsersResolve(response) {
-  return response.json();
+const options = {
+  results: 10,
+  seed: 'abc',
+  page: 1,
+};
+
+load(options);
+
+const [prevBtn, nextBtn] = document.querySelectorAll('button');
+prevBtn.addEventListener('click', btnPrevFun);
+nextBtn.addEventListener('click', btnNextFun);
+
+function btnPrevFun(e) {
+  if (options.page >= 1) options.page--;
+  load(options);
 }
 
-function logData(users) {
-  // обработка данных
-  console.log(users);
-  const list = document.getElementById('usersList');
+function btnNextFun(e) {
+  options.page++;
+  load(options);
+}
+
+function load({ results, seed, page }) {
+  fetch(
+    `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`
+  )
+    .then((response) => response.json())
+    .then(({ results }) => loadUsersOnPage(results));
+}
+
+function loadUsersOnPage(users) {
+  // проверить есть ли элемент с классом .list ?
+  // если есть то удалить старый список
+  // старый список - это список с прошлой страницы
+  const list = document.querySelector('.list');
+  if (list) list.remove();
+  // создание нового списка
+  const newList = document.createElement('ul');
+  newList.classList.add('list');
+  document.getElementById('usersCont').prepend(newList);
+  //новый элемент
+  //const usersArray = users.map((u) => createUserCard(u));
+  //newList.append(...usersArray);
   users.forEach((u) => {
-    list.append(createUserCard(u));
+    newList.append(createUserCard(u));
   });
 }
 
-function createUserCard(u) {
+function createUserCard({
+  name: { first: name, last: surname },
+  picture: { medium: img },
+}) {
   const item = document.createElement('li');
-  item.classList.add('userItem');
-
-  // image
-  const userImgCont = document.createElement('div');
-  userImgCont.classList.add('imgContainer');
-
-  const userImg = document.createElement('img');
-  userImg.setAttribute('src', u.img);
+  item.classList.add('item');
+  const userImg = new Image();
+  //const userImg = document.createElement('img');
+  userImg.setAttribute('src', img);
   userImg.setAttribute('alt', 'user photo');
   userImg.classList.add('userImg');
-
-  userImgCont.append(userImg);
-  item.append(userImgCont);
-  // text
+  item.append(userImg);
   const userFullName = document.createElement('p');
-  userFullName.innerText = `${u.name} ${u.surname}`;
+  userFullName.innerText = `${name} ${surname}`;
   userFullName.classList.add('userFullName');
   item.append(userFullName);
-
   return item;
 }
-*/
-
-/*
-const promise1 = new Promise(fun1);
-
-function fun1(resolve, reject) {
-  setTimeout(() => reject('err'), 2 * 1000);
-}
-
-function fun2(resolve, reject) {
-  setTimeout(() => resolve('ok'), 3 * 1000);
-}
-
-*/
-
-/*
-С помощью промиса setTimeout() : напишите функцию delay(ms),
-которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
-Должен быть возможен вот такой вызов delay(ms).then(doSomething)
-
-Пример использования и эквивалетный вызов setTimeout():
-
-funDelay(1000).then(() => alert("Hello!"))
-
-
-function sum (a,b) {}
-
-new Promise(function(resolve, reject) { ... });
-
-// **
-
-проверить при этом что число ms является действительно числом (тип Number)
-и если да то вернуть соответствующий пропис
-*/
-
-/*
-function funDelay(timeMS) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('ok');
-    }, timeMS);
-  });
-}
-
-funDelay(1 * 1000).then(() => alert('Hello'));
-*/
-
-/*
-function funDelay2(timeMS) {
-  if (!Number.isNaN(timeMS)) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('Promise have status "resolve"');
-      }, timeMS);
-    });
-  } else {
-    throw new Promise((resolve, reject) =>
-      setTimeout(() => reject(TypeError('Parameter is not a number'), timeMS))
-    );
-  }
-}
-
-//const ms = 2 * 1000;
-
-funDelay2(NaN)
-  .then((result) => console.log(result))
-  .catch((error) => {
-    console.log(error);
-  });
-*/
-
-/*
-Напишите функцию, которая последовательно выводит в консоль числа от 1 до 20,
- с интервалом между числами 100мс. То есть, весь вывод должен занимать 2000мс,
- в течение которых каждые 100мс в консоли появляется очередное число.
- Решение задачи должно использовать setTimeout. (По циклу таймеры не создавать)
- */
-
-/*
-//1 bad
-for (let i = 1; i <= 20; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, 100);
-}
-//2 !!!!
-let start = 1,
-  end = 20;
-const timerId = setInterval(() => {
-  start <= end ? console.log(start++) : clearInterval(timerId);
-}, 100);
-//3
-let i = 1;
-setTimeout(function funTime() {
-  console.log(i++);
-  if (i < 21) {
-    setTimeout(funTime, 100);
-  }
-}, 100);
-*/
-/*
-1. Создать promise для каждого типа (number, boolean, string, symbol, null, undefined, object )
-2. создать асинхронную функцию для их console.log
-*/
-
-/*
-const promise1 = Promise.resolve(100);
-const promise2 = Promise.resolve(true);
-const promise3 = Promise.resolve('Hello, Vasya!');
-const promise4 = Promise.resolve(Symbol('SOME_ACTION'));
-const promise5 = Promise.resolve(null);
-const promise6 = Promise.resolve(undefined);
-const promise7 = Promise.resolve({ id: 1, name: 'Vasya', age: 20 });
-*/
-/*
-console.log(promise1);
-promise1.then((result) => {
-  console.log('Result is', result);
-});
-*/
-/*
-console.log(promise7);
-
-promise7.then((data) => {
-  console.log('Result is', data);
-});
-
-console.log('----------------');
-async function promiseResolveValue(value) {
-  console.log('Value', await value);
-}
-
-promiseResolveValue(promise1);
-promiseResolveValue(promise7);
-*/
-
-/*
-// создать обещание на создание обьекта пользователь/машина/тел если булевая константа true
-
-let isAuth = true;
-
-let authUserFunction = new Promise(function (resolve, reject) {
-  if (isAuth) {
-    let newCreditCard = {
-      number: '1111000022223333',
-      data: '12/23',
-    };
-    resolve(newCreditCard);
-  } else {
-    reject(new Error('Problem with your auth'));
-  }
-});
-
-function getCard() {
-  authUserFunction
-    .then((result) => {
-      console.log('Result is', result);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-getCard();
-
-// асинхронно если хотим вызвать
-
-async function getCard2() {
-  try {
-    let newUserCreditCard = await authUserFunction;
-  } catch (error) {
-    close.log(error);
-  }
-}
-
-getCard2();
-*/
-
-// задача. динамически менять цвет фигуры
-
-const square = document.getElementById('square');
-
-// figura.style.backgroudColor
-
-//1
-/*
-function changeColorFun1(figure) {
-  //setInterval(() => (figure.style.backgroundColor = 'black'), 2000);
-  setTimeout(() => (figure.style.backgroundColor = 'black'), 2000);
-}
-
-changeColorFun1(square);
-*/
-
-/*
-//2
-
-function changeColorFun2() {
-  if (square.style.backgroundColor == 'red') {
-    square.style.backgroundColor = 'black';
-  } else {
-    square.style.backgroundColor = 'red';
-  }
-}
-
-setInterval(changeColorFun2, 2000);
-
-//3
-
-const container = document.getElementById('container')
-
-const color = ['green','blue','red','blue','green']
-let count = 0
-
-
-
-setInterval(function changeColor(){
-    container.style.backgroundColor = ${color[count++]}
-    console.log(count)
-},1000)
-*/
-
-//4
-
-let i = 0;
-function changeColor() {
-  square.style.backgroundColor = `rgb(${i++ % 256},0,0)`;
-}
-
-setInterval(changeColor, 10);
