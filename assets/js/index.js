9e6af0a9986b1309ac4c75fae6354a19a8f6878e
julietvt <freshitpt@gmail.com>
2021-11-28 10:30:54 +0200

    task promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/*
 fetch('../../assets/data/users.json')
   .then(loadUsersResolve)
   .then(logData)
@@ -39,3 +40,71 @@ function createUserCard(u) {
 
   return item;
 }
+*/
+
+/*
+const promise1 = new Promise(fun1);
+
+function fun1(resolve, reject) {
+  setTimeout(() => reject('err'), 2 * 1000);
+}
+
+function fun2(resolve, reject) {
+  setTimeout(() => resolve('ok'), 3 * 1000);
+}
+
+*/
+
+/*
+С помощью промиса setTimeout() : напишите функцию delay(ms),
+которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
+Должен быть возможен вот такой вызов delay(ms).then(doSomething)
+
+Пример использования и эквивалетный вызов setTimeout():
+
+funDelay(1000).then(() => alert("Hello!"))
+
+
+function sum (a,b) {}
+
+new Promise(function(resolve, reject) { ... });
+
+// **
+
+проверить при этом что число ms является действительно числом (тип Number)
+и если да то вернуть соответствующий пропис
+*/
+
+/*
+function funDelay(timeMS) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('ok');
+    }, timeMS);
+  });
+}
+
+funDelay(1 * 1000).then(() => alert('Hello'));
+*/
+
+function funDelay2(timeMS) {
+  if (!Number.isNaN(timeMS)) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Promise have status "resolve"');
+      }, timeMS);
+    });
+  } else {
+    throw new Promise((resolve, reject) =>
+      setTimeout(() => reject(TypeError('Parameter is not a number'), timeMS))
+    );
+  }
+}
+
+//const ms = 2 * 1000;
+
+funDelay2(NaN)
+  .then((result) => console.log(result))
+  .catch((error) => {
+    console.log(error);
+  });
